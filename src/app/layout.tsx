efe5b125db62fc626054978075ab4a2af1bce2db
Rahmankhattak711 <rahmankhattak796@gmail.com>
2024-12-16 22:54:57 +0500

    adding some change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Minimal Single Page Portfolio",
-  description: "Created with Frontend Tribe",
+  title: "Animated Landing Page",
+  description: "Animated Landing Page",
 };
 
 export default function RootLayout({
